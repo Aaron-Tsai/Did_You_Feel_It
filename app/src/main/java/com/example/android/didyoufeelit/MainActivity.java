069a5503a76317e55ff77f14fa9b2db85a363152
Aaron-Tsai <awrtsai@gmail.com>
2017-11-19 15:48:15 -0800

    Added NetworkRequest class

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -34,11 +35,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+        //these two statements create the ASyncInstance we need in order to make a network request in the background
+        //pass the String USGS_REQUEST_URL into the execute method to pass it as a parameter to doInBackground()
+        NetworkRequest fetchData = new NetworkRequest();
+        fetchData.execute(USGS_REQUEST_URL);
     }
 
     /**
@@ -53,5 +53,28 @@ public class MainActivity extends AppCompatActivity {
 
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+    private class NetworkRequest extends AsyncTask<String, Void, Event> {
+
+        //doInBackground executes when the execute() method is called by a NetworkRequest instance
+        protected Event doInBackground(String... url) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if (url.length < 1 || url[0] == null) {
+                return null;
+            }
+            // Perform the HTTP request for earthquake data and process the response.
+            return Utils.fetchEarthquakeData(url[0]);
+        }
+
+        //onPostExecute is called in the main thread after doInBackground() finishes
+        protected void onPostExecute(Event result) {
+            // If there is no result, do nothing.
+            if (result == null) {
+                return;
+            }
+            updateUi(result);
+        }
+
     }
 }
